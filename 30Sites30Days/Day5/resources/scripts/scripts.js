cbb5029114cc952582957c8d182c904606fc2ccc
Sean Newman <seangnewman@msn.com>
2018-01-28 00:06:32 -0700

    Add files via upload

---
+++ 30Sites30Days/Day5/resources/scripts/scripts.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+
+  $('.carousel').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      arrows: false,
+      fade: true
+    });
+});
